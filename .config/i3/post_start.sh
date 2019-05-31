05f15ff1d7c7879e44099f087872d515c93ee2ef
Josiah Salcido <commander225@gmail.com>
2019-05-31 08:53:57 -0700

    improve lock

--- .config/i3/post_start.sh
+++ .config/i3/post_start.sh
@@ -6,6 +6,10 @@
 /usr/bin/blueman-applet &
 /usr/bin/pasystray &
 /usr/bin/udiskie --no-notify --tray &
-/usr/bin/barrier &
+#/usr/bin/barrier &
+/usr/bin/riot-desktop --hidden &
+~/.config/i3lock/launch.sh &
+/usr/bin/gufw_icon.sh &
+/usr/bin/nm-applet &
 sleep 5 && /usr/bin/a2jmidid -e &
 #sleep 10 && /usr/bin/qjackctl &
